56c31610d12d131eea571fd67ac63d65a75c3f40
NagyGabor87 <gabor.nagy.aiesec@gmail.com>
2023-05-08 13:58:18 +0200

    add starting steps

---
+++ src/main/java/com/petproject/gamingwebsite/GamingWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.petproject.gamingwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamingWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamingWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/petproject/gamingwebsite/GamingWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petproject.gamingwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamingWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
