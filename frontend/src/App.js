986982018759d3d2c53182ae63c9338fbbc99e65
NagyGabor87 <gabor.nagy.aiesec@gmail.com>
2023-09-03 17:34:20 +0200

    add more modification to the frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,21 @@
 import React, {Component} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import {BrowserRouter as Router, Routes, Route}
     from 'react-router-dom';
 import HangmanGame from "./components/HangmanComponents/HangmanGame";
+import HomePage from "./components/MainComponents/HomePage";
+import BootstrapNavbar from "./components/MainComponents/BootstrapNavbar";
 
 class App extends Component {
     render() {
         return (
             <div className="App">
                 <Router>
+                    <BootstrapNavbar/>
                     <Routes>
-                        < Route path="/hangman" element={<HangmanGame/>}/>
+                        <Route path="/" element={<HomePage/>}/>
+                        <Route path="/hangman" element={<HangmanGame/>}/>
                     </Routes>
                 </Router>
             </div>
